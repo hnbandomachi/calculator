31540721829c65a921875e137bcfb7f4898b40b1
hnb <hnbandomachi@gmail.com>
2022-09-21 22:32:11 +0700

    completed computePostfix

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,23 +1,23 @@
 function Infix2Postfix(expression) {
-    // num . + - x ÷ ( ) sin( cos( tan(
-    // 5.2 + 4 x 6 - 3 x sin(3 + 4 x 5) + 3 x 7.14
+    // num . + - x ÷ ( ) sin cos tan
+    // 5.2 + 4 x 6 - 3 x sin3 + 4 x 5) + 3 x 7.14
     let number = "";
     let postfix = [];
     let stack = [];
     for (let index = 0; index < expression.length; index++) {
         let char = expression[index];
         let index_open =
-            stack.indexOf("(") >= 0? stack.indexOf("("):
-            stack.indexOf("sin(") >= 0? stack.indexOf("sin("):
-            stack.indexOf("cos(") >= 0? stack.indexOf("cos("):
-            stack.indexOf("tan(") >= 0? stack.indexOf("tan("): stack.length;
+            stack.indexOf("(") >= 0 ? stack.indexOf("(") :
+                stack.indexOf("sin") >= 0 ? stack.indexOf("sin") :
+                    stack.indexOf("cos") >= 0 ? stack.indexOf("cos") :
+                        stack.indexOf("tan") >= 0 ? stack.indexOf("tan") : stack.length;
         if (char === "+" || char === "-") {
             postfix.push(number);
             number = "";
             let indexPop =
-                stack.indexOf('sin(') >= 0 ? stack.indexOf('sin(') + 1 :
-                    stack.indexOf('cos(') >= 0 ? stack.indexOf('cos(') + 1 :
-                        stack.indexOf('tan(') >= 0 ? stack.indexOf('tan(') + 1 :
+                stack.indexOf('sin') >= 0 ? stack.indexOf('sin') + 1 :
+                    stack.indexOf('cos') >= 0 ? stack.indexOf('cos') + 1 :
+                        stack.indexOf('tan') >= 0 ? stack.indexOf('tan') + 1 :
                             stack.indexOf('+') >= 0 ? stack.indexOf('+') :
                                 stack.indexOf('-') >= 0 ? stack.indexOf('-') : stack.length;
 
@@ -32,7 +32,7 @@ function Infix2Postfix(expression) {
         } else if (char === "s" || char === "c" || char === "t") {
             index += 3;
             number = "";
-            stack.push(char === "s" ? "sin(" : char === "c" ? "cos(" : "tan(");
+            stack.push(char === "s" ? "sin" : char === "c" ? "cos" : "tan");
         } else if (char === "(" || char === ")") {
             if (char === "(") {
                 stack.push("(");
@@ -49,22 +49,63 @@ function Infix2Postfix(expression) {
     for (let i = stack.length; i > 0; i--) {
         postfix.push(stack.pop());
     }
-    console.log("postfix: ", postfix);
     return postfix;
 
     // 5.2 4 6 x + 3 3 4 5 x + sin x - 3 7.14 x +
 }
 
-function computePostfix(expression) { }
+function computePostfix(expression) {
+    // ['5.2', '4', '6', 'x', '+', '3', '3', '4', 'x', '+', 'sin', '5', 'x', '-', '3', '7.14', 'x', '+']
+    let stack = [];
+    for (let element of expression) {
+        if (isNaN(element)) {
+            switch (element) {
+                case '+':
+                    // Add
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+                case '-':
+                    // Minus
+                    stack.push(-stack.pop() + stack.pop());
+                    break;
+                case 'x':
+                    // Multiple
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+                case '÷':
+                    // Divide
+                    stack.push(1/stack.pop() * stack.pop());
+                    break;
+                case 'sin':
+                    // sin
+                    stack.push(Math.sin(stack.pop()));
+                    break;
+                case 'cos':
+                    // cos
+                    stack.push(Math.cos(stack.pop()));
+                    break;
+                case 'tan':
+                    // tan
+                    stack.push(Math.tan(stack.pop()));
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            stack.push(Number(element));
+        }
+    }
+    return stack[0];
+}
 
 export default function calculate(stateObj, btn) {
     if (btn === "AC") {
         stateObj.expression = "0";
     } else if (btn === "=") {
         console.log("Button = is clicked");
-        let postfixExpression = Infix2Postfix("5.2+4x6-3xsin(3+4x5)+3x7.14");
+        let postfixExpression = Infix2Postfix("5.2+4x6-3xsin3+4x5)+3x7.14");
         // let postfixExpression = Infix2Postfix(stateObj.expression);
-        // let result = computePostfix(postfixExpression);
+        let result = computePostfix(postfixExpression);
         stateObj.expression = "=";
     } else {
         stateObj.expression += btn;
